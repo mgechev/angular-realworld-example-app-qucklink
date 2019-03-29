25b00b349821e23cf4559568f3ccce1a295704d0
Minko Gechev <mgechev@google.com>
2019-03-28 17:02:38 -0700

    Update dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,9 +23,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    // preload all modules; optionally we could
-    // implement a custom preloading strategy for just some
-    // of the modules (PRs welcome 😉)
     preloadingStrategy: QuicklinkStrategy
   })],
   exports: [RouterModule]
